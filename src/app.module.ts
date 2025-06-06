8736b6fcf8814dec908abc5822d85b465cdb1a7f
hmbtaiwtm <immaculatekriss@gmail.com>
2025-06-06 17:37:30 +0200

    start

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthResolver } from './auth/auth.resolver';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [AuthModule],
+  controllers: [AppController],
+  providers: [AppService, AuthResolver, AuthService],
+})
+export class AppModule {}
